23b752aee0c96367859c5159b2f5a46fbb0ac293
Danny Fishman <danny.fishman1@gmail.com>
2018-03-04 13:45:57 -0500

    Update data.js

--- frontend/app/data.js
+++ frontend/app/data.js
@@ -8,20 +8,7 @@ export const songTracks = [];
 
 export const tourDates = [];
 
-export const videos = [
-  {
-    src: 'https://www.youtube.com/embed/OK5j3ds7qkQ',
-    title: "Honey Won't You Stay"
-  },
-  {
-    src: 'https://www.youtube.com/embed/ayd0gUK435Y',
-    title: "Shadow of a Man"
-  },
-  {
-    src: 'https://www.youtube.com/embed/P9AFh3b2tTw',
-    title: "Try Again"
-  }
-];
+export const videos = [];
 
 export const tabs = {
   bio: <Bio />,
